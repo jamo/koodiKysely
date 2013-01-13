d44516e5c48107a737ff89c5d2a5650762ce3cd0
Jarmo Isotalo <jamo@isotalo.fi>
2013-01-14 00:10:06 +0200

    joojoo

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
 class Student < ActiveRecord::Base
-  attr_accessible :details, :programmed_before, :student_number, :year, :hours, :language
+  attr_accessible :details, :programmed_before, :student_number, :year, :hours, :language, :gender
   has_many :code_questions #students answers
 end
